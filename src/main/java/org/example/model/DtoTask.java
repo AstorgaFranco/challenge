9c38ae661f321a7fb70280de392ee8dcfda00a65
punko6 <astorgapunko@gmail.com>
2023-02-14 22:39:58 -0300

    swagger

---
+++ src/main/java/org/example/model/DtoTask.java
@@ -0,0 +1,22 @@
+package org.example.model;
+
+public class DtoTask {
+    private String description;
+    private Long priority;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Long getPriority() {
+        return priority;
+    }
+
+    public void setPriority(Long priority) {
+        this.priority = priority;
+    }
+}
---
+++ src/main/java/org/example/model/Task.java
@@ -0,0 +1,42 @@
+package org.example.model;
+
+import org.springframework.lang.NonNull;
+
+import javax.persistence.*;
+
+@Entity
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String description;
+    @Column(name = "PRIORITY")
+    private Long priority;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId( Long id) {
+        this.id = id;
+    }
+
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription( String description) {
+        this.description = description;
+    }
+
+    public Long getPriority() {
+        return priority;
+    }
+
+    public void setPriority(Long priority) {
+        this.priority = priority;
+    }
+}
